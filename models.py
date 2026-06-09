164b6db56d43d41eab6988220745cbf3e88171b2
Ravi Tank <rtank2505@gmail.com>
2026-06-09 16:52:20 +0530

    Fix config

--- models.py
+++ models.py
@@ -1,24 +1,10 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Contact(db.Model):
-
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-
-    name = db.Column(
-        db.String(100)
-    )
-
-    email = db.Column(
-        db.String(100)
-    )
-
-    subject = db.Column(
-        db.String(200)
-    )
-
-    message = db.Column(
-        db.Text
-    )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    subject = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.Text, nullable=False)
