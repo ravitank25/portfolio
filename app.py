0e946860baa3d65d7171e9382eaba2ae61e6bad1
Ravi Tank <rtank2505@gmail.com>
2026-06-09 16:48:11 +0530

    Fix config

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portfolio.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db.init_app(app) 
 
 
 @app.route("/")
