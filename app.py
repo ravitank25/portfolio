866ff1353cef48b7e5080c2988881c3e492d634f
Ravi Tank <rtank2505@gmail.com>
2026-06-09 19:44:12 +0530

    Fix config

--- app.py
+++ app.py
@@ -7,28 +7,27 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/contacts.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app) 
+db.init_app(app)
 
+@app.before_request
+def create_tables():
+    db.create_all()
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
 @app.route("/about")
 def about():
     return render_template("about.html")
-
 
 @app.route("/skills")
 def skills():
     return render_template("skills.html")
 
-
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
-
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
@@ -39,39 +38,18 @@ def contact():
             subject=request.form.get("subject"),
             message=request.form.get("message")
         )
-
         db.session.add(contact)
         db.session.commit()
-
         return redirect("/thankyou")
-
     return render_template("contact.html")
-
 
 @app.route("/thankyou")
 def thankyou():
     return render_template("thankyou.html")
 
-
 @app.route("/resume")
 def resume():
-    return send_file(
-        "static/resume/resume.pdf",
-        as_attachment=True
-    )
-
-
-# @app.route("/admin")
-# def admin():
-#     contacts = Contact.query.all()
-#     return render_template("admin.html", contacts=contacts)
-
-
-# Create database tables
-@app.before_request
-def create_tables():
-    db.create_all()
-
+    return send_file("static/resume/resume.pdf", as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
